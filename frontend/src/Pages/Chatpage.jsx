6c9714856c3865114cc6ef25c8a9bdf2b99e351a
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-11 14:01:53 +0300

    providing fetching contexts from chatprovider.jsx

--- frontend/src/Pages/Chatpage.jsx
+++ frontend/src/Pages/Chatpage.jsx
@@ -1,9 +1,14 @@
 import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
 import Chatbox from "../components/Chatbox";
 import MyChats from "../components/MyChats";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
 
 const Chatpage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+
   return (
     <div style={{ width: "100%", flexDirection: "row" }}>
       {user && <SideDrawer />}
@@ -14,8 +19,10 @@ const Chatpage = () => {
         h="91.5vh"
         p="10px"
       >
-        {user && <MyChats />}
-        {user && <Chatbox />}
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
   );
