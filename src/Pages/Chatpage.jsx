e15aff023a09943b4a5926876dec193993dccf61
tabby400 <125541304+tabby400@users.noreply.github.com>
2023-12-11 12:52:37 +0300

    Add files via upload
    src files

---
+++ src/Pages/Chatpage.jsx
@@ -0,0 +1,24 @@
+import { Box } from "@chakra-ui/layout";
+import Chatbox from "../components/Chatbox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+
+const Chatpage = () => {
+  return (
+    <div style={{ width: "100%", flexDirection: "row" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats />}
+        {user && <Chatbox />}
+      </Box>
+    </div>
+  );
+};
+
+export default Chatpage;
