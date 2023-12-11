2db463a82bee6de8b9f28ad72b890eb190685958
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-11 13:45:44 +0300

    grabbing data from backend

--- frontend/src/components/miscellaneous/ProfileModal.jsx
+++ frontend/src/components/miscellaneous/ProfileModal.jsx
@@ -14,21 +14,25 @@ import {
   Image,
 } from "@chakra-ui/react";
 
-const ProfileModal = () => {
+const ProfileModal = ({ user, children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton
+          display={{ base: "flex" }}
+          icon={<ViewIcon />}
+          onClick={onOpen}
+        />
+      )}
       <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
         <ModalContent h="410px">
-          <ModalHeader
-            fontSize="40px"
-            fontFamily="Work sans"
-            display="flex"
-            justifyContent="center"
-          >
-            {/* {user.name} */}
+          <ModalHeader fontSize="40px" display="flex" justifyContent="center">
+            {user.name}
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody
@@ -43,10 +47,7 @@ const ProfileModal = () => {
               src={user.pic}
               alt={user.name}
             />
-            <Text
-              fontSize={{ base: "28px", md: "30px" }}
-              fontFamily="Work sans"
-            >
+            <Text fontSize={{ base: "28px", md: "30px" }}>
               Email: {user.email}
             </Text>
           </ModalBody>
